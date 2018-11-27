59280d9c877140612f6fca9cef2222d9c7f83d0e
chenfromsz <mrcsj@tom.com>
2018-11-27 18:47:20 +0800

    V2.0

--- src/main/java/springboot/example/Application.java
+++ src/main/java/springboot/example/Application.java
@@ -2,18 +2,21 @@ package springboot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
 public class Application {
-    @RequestMapping("/")
-    String home() {
-        return "hello";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello!";
+    }
+
 }
